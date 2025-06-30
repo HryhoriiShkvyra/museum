0f55304aa5ced55309fbca441bfcfdff92b855a8
Hryhorii Shkvyra <52300830+HryhoriiShkvyra@users.noreply.github.com>
2025-06-30 13:59:22 +0300

    fixes

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -18,33 +18,125 @@ export default function Header() {
     <div className="navbar-wrapper">
       <TopBar />
 
-      <div className="section">
-        <div className="navbar">
-          <Link onClick={(e) => setMobileNavBar(false)} to="/">
-            <img className="logo" src={Logo} alt="" />
-          </Link>
-          <div className="navbar-btns">
-            <div className="navbar-links">
-              <Link to="/about-museum" className="navbar-link">
-                <h4>про музей</h4>
-              </Link>
-              <Link to="/museum-tour" className="navbar-link">
-                <h4>прогулянки музеєм</h4>
-              </Link>
-              <Link to="/catalog" className="navbar-link">
-                <h4>експонати музею</h4>
-              </Link>
-              <Link to="/gallery" className="navbar-link">
-                <h4>галерея</h4>
-              </Link>
-              <Link to="/news" className="navbar-link">
-                <h4>про нас</h4>
-              </Link>
-              <Link to="/contact-us" className="navbar-link">
-                <h4>контакти</h4>
-              </Link>
-            </div>
+      <div className="navbar">
+        <Link onClick={(e) => setMobileNavBar(false)} to="/">
+          <img className="logo" src={Logo} alt="" />
+        </Link>
+        <div className="navbar-btns">
+          <div className="navbar-links">
+            <Link to="/about-museum" className="navbar-link">
+              <h4>про музей</h4>
+            </Link>
+            <Link to="/museum-tour" className="navbar-link">
+              <h4>прогулянки музеєм</h4>
+            </Link>
+            <Link to="/catalog" className="navbar-link">
+              <h4>експонати музею</h4>
+            </Link>
+            <Link to="/gallery" className="navbar-link">
+              <h4>галерея</h4>
+            </Link>
+            <Link to="/news" className="navbar-link">
+              <h4>про нас</h4>
+            </Link>
+            <Link to="/contact-us" className="navbar-link">
+              <h4>контакти</h4>
+            </Link>
+          </div>
 
+          <div className="navbar-lang-btns">
+            <div
+              onClick={(e) => setLangActive("UA")}
+              className={
+                langActive === "UA"
+                  ? "navbar-lang-btn-wrapper-active"
+                  : "navbar-lang-btn-wrapper"
+              }
+            >
+              <div className="navbar-lang-btn">
+                <h3>UA</h3>
+              </div>
+            </div>
+            <div
+              onClick={(e) => setLangActive("EN")}
+              className={
+                langActive === "EN"
+                  ? "navbar-lang-btn-wrapper-active"
+                  : "navbar-lang-btn-wrapper"
+              }
+            >
+              <div className="navbar-lang-btn">
+                <h3>EN</h3>
+              </div>
+            </div>
+            <div
+              onClick={(e) => setLangActive("RU")}
+              className={
+                langActive === "RU"
+                  ? "navbar-lang-btn-wrapper-active"
+                  : "navbar-lang-btn-wrapper"
+              }
+            >
+              <div className="navbar-lang-btn">
+                <h3>RU</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-mobile">
+          <button onClick={mobileNavbarToggle} className="navbar-mobile-icon">
+            <DehazeRoundedIcon style={{ color: "var(--white)" }} />
+          </button>
+        </div>
+      </div>
+      {mobileNavbar ? (
+        <div className="navbar-mobile-wrapper">
+          <div className="navbar-mobile-btns">
+            <Link
+              onClick={(e) => setMobileNavBar((prev) => !prev)}
+              to="/about-museum"
+              className="navbar-mobile-btn"
+            >
+              <h4>ПРО МУЗЕЙ</h4>
+            </Link>
+            <Link
+              onClick={(e) => setMobileNavBar((prev) => !prev)}
+              to="/museum-tour"
+              className="navbar-mobile-btn"
+            >
+              <h4>ПРОГУЛЯНКИ МУЗЕЕМ</h4>
+            </Link>
+            <Link
+              onClick={(e) => setMobileNavBar((prev) => !prev)}
+              to="/catalog"
+              className="navbar-mobile-btn"
+            >
+              <h4>ЕКСПОНАТИ МУЗЕЮ</h4>
+            </Link>
+            <Link
+              onClick={(e) => setMobileNavBar((prev) => !prev)}
+              to="/gallery"
+              className="navbar-mobile-btn"
+            >
+              <h4>ГАЛЕРЕЯ</h4>
+            </Link>
+            <Link
+              onClick={(e) => setMobileNavBar((prev) => !prev)}
+              to="/news"
+              className="navbar-mobile-btn"
+            >
+              <h4>ПРО НАС</h4>
+            </Link>
+            <Link
+              onClick={(e) => setMobileNavBar((prev) => !prev)}
+              to="/contact-us"
+              className="navbar-mobile-btn"
+            >
+              <h4>КОНТАКТИ</h4>
+            </Link>
+          </div>
+          <div className="navbar-mobile-lang-btns">
+            {" "}
             <div className="navbar-lang-btns">
               <div
                 onClick={(e) => setLangActive("UA")}
@@ -84,102 +176,8 @@ export default function Header() {
               </div>
             </div>
           </div>
-          <div className="navbar-mobile">
-            <button onClick={mobileNavbarToggle} className="navbar-mobile-icon">
-              <DehazeRoundedIcon style={{ color: "var(--white)" }} />
-            </button>
-          </div>
         </div>
-        {mobileNavbar ? (
-          <div className="navbar-mobile-wrapper">
-            <div className="navbar-mobile-btns">
-              <Link
-                onClick={(e) => setMobileNavBar((prev) => !prev)}
-                to="/about-museum"
-                className="navbar-mobile-btn"
-              >
-                <h4>ПРО МУЗЕЙ</h4>
-              </Link>
-              <Link
-                onClick={(e) => setMobileNavBar((prev) => !prev)}
-                to="/museum-tour"
-                className="navbar-mobile-btn"
-              >
-                <h4>ПРОГУЛЯНКИ МУЗЕЕМ</h4>
-              </Link>
-              <Link
-                onClick={(e) => setMobileNavBar((prev) => !prev)}
-                to="/catalog"
-                className="navbar-mobile-btn"
-              >
-                <h4>ЕКСПОНАТИ МУЗЕЮ</h4>
-              </Link>
-              <Link
-                onClick={(e) => setMobileNavBar((prev) => !prev)}
-                to="/gallery"
-                className="navbar-mobile-btn"
-              >
-                <h4>ГАЛЕРЕЯ</h4>
-              </Link>
-              <Link
-                onClick={(e) => setMobileNavBar((prev) => !prev)}
-                to="/news"
-                className="navbar-mobile-btn"
-              >
-                <h4>ПРО НАС</h4>
-              </Link>
-              <Link
-                onClick={(e) => setMobileNavBar((prev) => !prev)}
-                to="/contact-us"
-                className="navbar-mobile-btn"
-              >
-                <h4>КОНТАКТИ</h4>
-              </Link>
-            </div>
-            <div className="navbar-mobile-lang-btns">
-              {" "}
-              <div className="navbar-lang-btns">
-                <div
-                  onClick={(e) => setLangActive("UA")}
-                  className={
-                    langActive === "UA"
-                      ? "navbar-lang-btn-wrapper-active"
-                      : "navbar-lang-btn-wrapper"
-                  }
-                >
-                  <div className="navbar-lang-btn">
-                    <h3>UA</h3>
-                  </div>
-                </div>
-                <div
-                  onClick={(e) => setLangActive("EN")}
-                  className={
-                    langActive === "EN"
-                      ? "navbar-lang-btn-wrapper-active"
-                      : "navbar-lang-btn-wrapper"
-                  }
-                >
-                  <div className="navbar-lang-btn">
-                    <h3>EN</h3>
-                  </div>
-                </div>
-                <div
-                  onClick={(e) => setLangActive("RU")}
-                  className={
-                    langActive === "RU"
-                      ? "navbar-lang-btn-wrapper-active"
-                      : "navbar-lang-btn-wrapper"
-                  }
-                >
-                  <div className="navbar-lang-btn">
-                    <h3>RU</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : null}
-      </div>
+      ) : null}
     </div>
   );
 }
--- src/TopBar/TopBar.jsx
+++ src/TopBar/TopBar.jsx
@@ -5,24 +5,22 @@ import PhoneIcon from "@mui/icons-material/Phone";
 
 function TopBar() {
   return (
-    <div className="section">
-      <div className="top-bar">
-        <div className="top-bar-links">
-          <div className="top-bar-link">
-            <div className="top-bar-link-icon">
-              <PlaceIcon />
-            </div>
-            <div className="top-bar-link-text">
-              <h4>м. Київ, Вознесенський узвіз, 00</h4>
-            </div>
+    <div className="top-bar">
+      <div className="top-bar-links">
+        <div className="top-bar-link">
+          <div className="top-bar-link-icon">
+            <PlaceIcon />
           </div>
-          <div className="top-bar-link">
-            <div className="top-bar-link-icon">
-              <PhoneIcon />
-            </div>
-            <div className="top-bar-link-text">
-              <h4>+38 (000) 000-00-00</h4>
-            </div>
+          <div className="top-bar-link-text">
+            <h4>м. Київ, Вознесенський узвіз, 00</h4>
+          </div>
+        </div>
+        <div className="top-bar-link">
+          <div className="top-bar-link-icon">
+            <PhoneIcon />
+          </div>
+          <div className="top-bar-link-text">
+            <h4>+38 (000) 000-00-00</h4>
           </div>
         </div>
       </div>
